3e7f727200bd277688653b596bc207662be7dc7f
Swetanjal Murati Dutta <swetanjaldatta@gmail.com>
2017-09-27 13:33:40 +0530

    Update

--- iiit_vpn_setup.sh
+++ iiit_vpn_setup.sh
@@ -4,7 +4,7 @@ if [ -f /etc/openvpn/ca.crt ]
 	echo "Connecting to IIITH VPN..."
 	echo "Press any key to continue."
 	read random
-	sudo bash vpn.sh && sudo bash vpn_exit.sh
+	sudo bash ./IIITH-VPN/vpn.sh && sudo bash ./IIITH/vpn_exit.sh
 	echo "You have successfully disconnected."
 
 else
